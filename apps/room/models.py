84b47e2b07cc4bbd5b3116f7a11b859111a4873c
alex <eddyalex.dev@gmail.com>
2022-07-24 13:20:38 -0400

    se agrego el get para las habitaciones

--- apps/room/models.py
+++ apps/room/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    rooms_id = ( ("A1", "A1"), ("A2", "A2"), ("B1", "B1"), ("B2", "B2") )
+
+    location_room = models.CharField(max_length=10, choices=rooms_id)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.location_room
+
--- apps/room/views.py
+++ apps/room/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+#Room
+from .models import Room
+from .serializers import RoomSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def getRooms(request):
+    if Room.objects.all().exists():
+        rooms = Room.objects.all()
+        serializer = RoomSerializer(rooms, many=True)
+        return Response({ "rooms": serializer.data }, status=status.HTTP_200_OK)
+    return Response({ "error": "Error al cargar las habitaciones" }, status=status.HTTP_400_BAD_REQUEST)
